549672b8ae4f5f268cdaa17e47a863b27595ae19
meilbn <codingallnight@gmail.com>
2016-10-13 16:40:44 +0800

    update demo project

---
+++ MLBOmniButtonDemo/MLBOmniButtonDemo/ViewControllers/MLBAutoLayoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLBAutoLayoutViewController.h
+//  MLBOmniButtonDemo
+//
+//  Created by meilbn on 13/10/2016.
+//  Copyright © 2016 meilbn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLBAutoLayoutViewController : UIViewController
+
+@end
---
+++ MLBOmniButtonDemo/MLBOmniButtonDemo/ViewControllers/MLBFrameViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLBFrameViewController.h
+//  MLBOmniButtonDemo
+//
+//  Created by meilbn on 13/10/2016.
+//  Copyright © 2016 meilbn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLBFrameViewController : UIViewController
+
+@end
